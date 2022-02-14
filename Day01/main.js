1b2ed9bb4e10099a12ae858871204c9046b6b810
Jose Omar Landa <79344240+omar-landa@users.noreply.github.com>
2022-02-14 17:02:44 -0600

    Create main.js

---
+++ Day01/main.js
@@ -0,0 +1,14 @@
+let firstName = 'Jose'
+let lastName = 'Landa'
+let county = "United States"
+
+console.log(firstName, lastName, county)
+
+//Comments can make code readable
+
+//Welcome to 30DaysOfJavaScript
+
+/*Comments can make code readable,
+easy to use,
+and informative */
+
