1fab1fecd8cc94d8b4d82bf2c1e2cad70bc3b66a
Jose Omar Landa <79344240+omar-landa@users.noreply.github.com>
2022-02-14 17:03:07 -0600

    Create variable.js

---
+++ Day01/variable.js
@@ -0,0 +1,14 @@
+let dogName = "Googie"
+let catName = "Tito"
+let isDog = true
+let isDefined = undefined
+let isNull = null
+
+console.log(dogName, catName, isDog, isDefined, isNull)
+
+
+let person = "people",
+dogBreed = "chihuahua",
+catType = "Stray"
+
+console.log(person, dogBreed, catType)
